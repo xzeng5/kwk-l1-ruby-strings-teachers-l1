1f25ca167e9eac04e1f3fbbabd7b388ab08ac91c
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-12 22:02:54 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,7 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-puts "Hello, what's your name?"
+puts "Hi, you've been invited to a party! What is your name?"
 
 guest_name = gets.chomp.capitalize 
 
